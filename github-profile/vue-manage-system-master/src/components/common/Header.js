c4381376ecaa57456be8372d3f37f944ae3f951b
xuchuangyu <254568214@qq.com>
2020-08-21 17:55:55 +0800

    111

--- github-profile/vue-manage-system-master/src/components/common/Header.js
+++
@@ -1,6 +0,0 @@
-import require from '../../utils/request'
-export default {
-    queryMessage(params){
-        return  require.get('/v1/comment',{params:{articleId:99999,read:-1,recycle:-1}})
-    },
-}
